0326570e642c2771f1ef89d0572b642ada666bce
Thiago de Matos <thiago.dematos.1@gmail.com>
2023-03-18 16:08:21 -0400

    add index.html

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let playerScore = 0;
+let compScore = 0;
 
 function getComputerChoice() {
     const randomChoice = ['rock', 'paper', 'scissor'];
@@ -7,37 +9,45 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-
-    if (playerSelection === 'rock' && computerSelection === 'rock') {
-        return 'May-Day! You a tied!'
+    console.log('1', playerSelection, '2', computerSelection)
+    if (playerSelection === computerSelection) {
+        return `May-Day! You a tied! ${playerSelection}`
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        compScore++
         return 'You lose this one :('
-    } else if (playerSelection === 'rock' && computerSelection === 'Scissor') {
+    } else if (playerSelection === 'rock' && computerSelection === 'scissor') {
+        playerScore++
         return 'Congradulations You Win!'
-    }else if (playerSelection === 'paper' && computerSelection === 'paper') {
-        return 'May-Day! You a tied!'
-    } else if (playerSelection === 'paper' && computerSelection === 'Scissor') {
+    } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
+        compScore++
         return 'You lose this one :('
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++
         return 'Congradulations You Win!'
     }else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+        playerScore++
         return 'Congradulations You Win!'
-    } else if (playerSelection === 'scissor' && computerSelection === 'Scissor') {
-        return 'May-Day! You a tied!'
     } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
+        compScore++
         return 'You lose this one :('
+    }
 }
 
-let playerSelection = 'playerChoice';
-playRound(playerSelection)
-//create a function for the player portion of the game
-// name 3 variables that will be used for the game
-//prompt the player to type an answer
 
-//if the player types rock
-//alert you win
-//if the player types paper
-//alert you win
-//if the player types scissor
-//alert you win
-//else type tied
+function game() {
+    for (let i = 1; i < 5; i++) {
+        const playerSelection = prompt('Chooser your weapon', 'Rock, Paper, Scissor');
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection.toLowerCase(), computerSelection);
+    }
+
+    if (playerScore > compScore) {
+        return "You beat the computer!"
+    } else if (playerScore < compScore) {
+        return "You lost to a computer? Bruh!"
+    } else { 
+        return " You tied, tough luck!"
+    }
+}
+
+console.log(game());
